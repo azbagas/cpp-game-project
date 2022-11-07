04026008c772b7c7c304b51341f2280e7a31b7d0
David Lois <107032089+DvaDvaD@users.noreply.github.com>
2022-11-07 13:50:06 +0700

    Update main.cpp
    Untuk memperjelas pembagian tugas.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+// Struct Musuh Dan Pemain
+
+
+// Prototipe Fungsi
+void startPage();
+
 
 int main() {
     cout << "Hello guys";
+    cout << "halo juga gaes";
 
     return 0;
+}
+
+// Definisi Fungsi
+void startPage(){
+    
 }
